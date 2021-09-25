6c7900d0bd30e9cb6bf1d50ecd239663df2d91b1
Anand Pap <anandpap@live.com>
2021-09-25 05:49:50 +0200

    Countdown

---
+++ ex02/src/index.js
@@ -0,0 +1,13 @@
+// Only change code below this line
+function myCounter(n) {
+    if (n <= 0) {
+        return [];
+    } else {
+        const countArray = myCounter(n - 1);
+        countArray.unshift(n);
+        return countArray;
+    }
+}
+// Only change code above this line
+console.log(myCounter(5)); // Change this line
+module.exports = myCounter;
