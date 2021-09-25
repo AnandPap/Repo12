dc5cf5d99720c136f264ca700a795f02ce0af9b1
Anand Pap <anandpap@live.com>
2021-09-25 06:24:36 +0200

    Range of numbers

---
+++ ex03/src/index.js
@@ -0,0 +1,16 @@
+// Only change code below this line
+function rangeOfNumbers(startN, endN) {
+    if (endN < startN) {
+        return "The starting number will always be less than or equal to the ending number";
+    }
+    if (endN == startN) {
+        return [startN];
+    } else {
+        const middleArray = rangeOfNumbers(startN, endN - 1);
+        middleArray.push(endN);
+        return middleArray;
+    }
+}
+// Only change code above this line
+console.log(rangeOfNumbers(10, 10)); // Change this line
+module.exports = rangeOfNumbers;
